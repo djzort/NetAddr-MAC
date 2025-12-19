2752d08de82ed5fbbbe4828d58e21df08ca7f252
Dean Hamstead <dean@fragfest.com.au>
2025-12-20 00:12:40 +1100

    Rework tokenring calculation

--- lib/NetAddr/MAC.pm
+++ lib/NetAddr/MAC.pm
@@ -16,29 +16,6 @@ use constant EUI48LENGTHDEC => 6;
 use constant EUI64LENGTHHEX => 16;
 use constant EUI64LENGTHDEC => 8;
 
-use constant ETHER2TOKEN => (
-## see also http://www-01.ibm.com/support/docview.wss?uid=nas114157020a771b25d862567250003b62c
-## note this table is rotated compared to the above link,
-## so that the hex values line up as a linear array :)
-## 0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
-    qw(00 80 40 c0 20 a0 60 e0 10 90 50 d0 30 b0 70 f0),    # 0
-    qw(08 88 48 c8 28 a8 68 e8 18 98 58 d8 38 b8 78 f8),    # 1
-    qw(04 84 44 c4 24 a4 64 e4 14 94 54 d4 34 b4 74 f4),    # 2
-    qw(0c 8c 4c cc 2c ac 6c ec 1c 9c 5c dc 3c bc 7c fc),    # 3
-    qw(02 82 42 c2 22 a2 62 e2 12 92 52 d2 32 b2 72 f2),    # 4
-    qw(0a 8a 4a ca 2a aa 6a ea 1a 9a 5a da 3a ba 7a fa),    # 5
-    qw(06 86 46 c6 26 a6 66 e6 16 96 56 d6 36 b6 76 f6),    # 6
-    qw(0e 8e 4e ce 2e ae 6e ee 1e 9e 5e de 3e be 7e fe),    # 7
-    qw(01 81 41 c1 21 a1 61 e1 11 91 51 d1 31 b1 71 f1),    # 8
-    qw(09 89 49 c9 29 a9 69 e9 19 99 59 d9 39 b9 79 f9),    # 9
-    qw(05 85 45 c5 25 a5 65 e5 15 95 55 d5 35 b5 75 f5),    # a
-    qw(0d 8d 4d cd 2d ad 6d ed 1d 9d 5d dd 3d bd 7d fd),    # b
-    qw(03 83 43 c3 23 a3 63 e3 13 93 53 d3 33 b3 73 f3),    # c
-    qw(0b 8b 4b cb 2b ab 6b eb 1b 9b 5b db 3b bb 7b fb),    # d
-    qw(07 87 47 c7 27 a7 67 e7 17 97 57 d7 37 b7 77 f7),    # e
-    qw(0f 8f 4f cf 2f af 6f ef 1f 9f 5f df 3f bf 7f ff),    # f
-);
-
 our %EXPORT_TAGS;
 
 %EXPORT_TAGS = (
@@ -932,7 +909,13 @@ Returns the mac address normalized as a hexadecimal string that is 0 padded and 
 sub as_tokenring {
 
     my $self = shift;
-    return join( q{-}, map { (ETHER2TOKEN)[$_] } @{ $self->{mac} } )
+
+    return join '-', map {
+        my $byte = $_;
+        my $rev  = 0;
+        $rev = ($rev << 1) | ($byte & 1), $byte >>= 1 for 1..8;
+        sprintf '%02x', $rev
+    } @{ $self->{mac} };
 }
 
 =head2 to_eui48
